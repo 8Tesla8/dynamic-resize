b2d88eb66a661d36096e405558aba1793cebf49a
tesla <kela256@gmail.com>
2021-11-16 20:53:20 +0200

    add resize example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-project';
+
+  public height: number;
+
+  constructor() {
+    this.onResize(null); 
+  }
+
+  //Supported global targets are window, document, and body.
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    let tenPercentHeight = (10/100) * window.innerHeight; 
+    this.height = window.innerHeight - tenPercentHeight;
+  }
+
 }
